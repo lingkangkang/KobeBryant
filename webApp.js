4a53500a17ad325d5f9819da6831d2b06338e048
lingkangkang <2492614819@qq.com>
2017-03-21 10:41:13 +0800

    second

--- webApp.js
+++ webApp.js
@@ -1,21 +1,15 @@
 /**
  * Created by Administrator on 2017/3/16.
  */
+$(function(){
 //touch event
 $('.footContent .footBody label').on('touchstart',function(e) {
-   console.log("ssss111")
     $(this).addClass("touchStart")
-    //$(this).css({
-    //    "background-color":"wheat",
-    //    "color":"black"
-    //})
 });
 $('.footContent .footBody label').on('touchmove',function(e) {
-    console.log("ssss2222")
     $(this).removeClass("touchStart")
 })
 $('.footContent .footBody label').on('touchend',function(e) {
-    console.log("ssss3333")
     $(this).removeClass("touchStart")
 })
 
@@ -23,7 +17,27 @@ $('.footContent .footBody label').on('touchend',function(e) {
 $(".footContent .footBody label").on("swipeleft",function(){
     console.log("you can not swipe left")
 })
-//固定footer
+//footer
 $(".footContent .footBody label").eq(0).on("click",function(){
-    alert("111")
+    alert("黝黑蜗壳 还没开发好")
+})
+$(".footContent .footBody label").eq(1).on("click",function(){
+    alert("黝黑蜗壳 还没开发好")
+})
+$(".footContent .footBody label").eq(2).on("click",function(){
+    alert("黝黑蜗壳 还没开发好")
+})
+$(".footContent .footBody label").eq(3).on("click",function(){
+    alert("黝黑蜗壳 还没开发好")
+    })
+
+//表头颜色变化
+ var colorVally=['yellow','white','black','blue','yellow','white','black','blue','yellow','blue']
+ setInterval(function(){
+     var numbers=Math.ceil(Math.random()*10)
+     $(".headContent p ").css({
+         "color":colorVally[numbers]
+     })
+ },2000)
+
 })
